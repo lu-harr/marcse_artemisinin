887572dca932f34049e7058196f56b257b4ec470
Lucinda Harrison <lucy.harrison.research@gmail.com>
2025-07-15 09:41:43 +0100

    CI width uncertainty supp?

--- code/power.R
+++ code/power.R
@@ -3,7 +3,7 @@ pwr_binom <- function(n, p){
   1 - (1 - p)^n
 }
 
-p <- preds$`2022_post_mean`
+p <- preds$`2022_post_median`
 
 # from surveillance_effort
 gf <- focalMat(ras, 1, "Gauss")
@@ -31,13 +31,18 @@ plot(pwr, main="Power")
 
 wald_ci <- function(n, p, alpha){
   z <- qnorm(alpha)
-  plot(sqrt(p * (1 - p) / n))
-  p + c(1, -1) * z * sqrt(p * (1 - p) / n)
+  #plot(sqrt(p * (1 - p) / n))
+  pm <- z * sqrt(p * (1 - p) / n)
+  return(list(upper = p + pm,
+              lower = p - pm))
 }
 
 wilson_ci <- function(n, p, alpha){
   z <- qnorm(alpha)
-  return((1 + z**2/n)**(-1)*(p + z**2/(2*n) + c(-1, 1)*(z / (2*n) * sqrt(4*n*p*(1 - p) + z**2))))
+  centre <- p + z**2/(2*n)
+  pm <- (z / (2*n) * sqrt(4*n*p*(1 - p) + z**2))
+  return(list(upper = (1 + z**2/n)**(-1)*(p + z**2/(2*n) + pm),
+              lower = (1 + z**2/n)**(-1)*(p + z**2/(2*n) - pm)))
 }
 
 tmp <- wald_ci(n, p, 0.9)
@@ -46,10 +51,10 @@ tmp <- wald_ci(n, p, 0.9)
 tmp <- wilson_ci(n, p, 0.95)
 
 par(mfrow = c(1,3))
-plot(tmp$lyr1, main = "Lower")
-plot(tmp$lyr2, main = "Upper")
+plot(tmp$lower, main = "Lower")
+plot(tmp$upper, main = "Upper")
 # this is what I'm trying to get to!
-plot(tmp$lyr2 - tmp$lyr1, main = "Confidence interval width")
+plot(tmp$upper - tmp$lower, main = "Confidence interval width")
 # looks quite a bit like 1/sqrt(n), so p isn't doing too much?
 # would still like to get to the idea that we know *less* where our estimated p is low?
 plot(1/sqrt(n))
@@ -58,8 +63,8 @@ plot(1/sqrt(n))
 # no idea what's going on down here I'm cooked but can't trust outputs until I figure it out
 ppp <- seq(0,1, length.out = 100)
 tmp <- wilson_ci(100, ppp, 0.95)
-plot(ppp, tmp[seq])
-
+plot(ppp, tmp$upper, type="l")
+lines(ppp, tmp$lower)
 
 
 
